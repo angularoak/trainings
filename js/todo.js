1637c2cb43dfad2d354ac7ae52d688fc6ab2a1d7
Suneel Manyam <hmanyam@prokarma.com>
2016-04-26 01:58:01 -0500

    Added coorect logic to work todo

--- js/todo.js
+++ js/todo.js
@@ -2,16 +2,17 @@ var app = angular.module("myApp",[]);
 
 app.controller("todo",function($scope){
     var tasks=[{
-    "desc":"Go to Training"
+        "desc":"Go to Training"
     },{
-    "desc":"Learn Angular"
+        "desc":"Learn Angular"
     },
-    {
-    "desc":"Complete HTML,CSS"
-    }];
-    function addTask(task){
-       tasks.push(task);
-     };
+               {
+                   "desc":"Complete HTML,CSS"
+               }];
+    function addTask(){
+        $scope.tasks.push($scope.todoTask);
+        $scope.todoTask={};
+    };
     $scope.tasks=tasks;
     $scope.addTask = addTask;
 });
