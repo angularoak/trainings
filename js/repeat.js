4317322e9e86143efd00f78e34e97d57b75ffe0e
Suneel Manyam <hmanyam@prokarma.com>
2016-04-26 07:24:54 -0500

    avoid duplicate todo tasks

--- js/repeat.js
+++ js/repeat.js
@@ -1,24 +1,10 @@
 var app = angular.module("repeatExample",[]);
 
-app.controller("userDetails",function($scope){
-    var persons=[{
-	"FirstName": "Suneel",
-	"LastName": "Manyam",
-	"imageUrl": "https://goo.gl/56lHky",
-	"twitterUrl": "https://twitter.com/sunilhari9"
-	
-}, {
-	"FirstName": "Swarna",
-	"LastName": "Yenugu",
-	"imageUrl": "https://goo.gl/56lHky",
-    "twitterUrl": "https://twitter.com/urstrulyMahesh"
-}, {
-	"FirstName": "Revathi",
-	"LastName": "Yalamala",
-	"imageUrl": "https://goo.gl/56lHky",
-    "twitterUrl": "https://twitter.com/ssrajamouli"
-}]
-    
+app.controller("userDetails",function($scope,$http){
+
+    $http.get("http://www.w3schools.com/angular/customers.php").then(function (response) {
+        $scope.persons = response.data.records;
+    });
     $scope.userName="Suneel";
-    $scope.persons=persons;
+
 });
--- js/todo.js
+++ js/todo.js
@@ -5,14 +5,19 @@ app.controller("todo",function($scope){
         "desc":"Go to Training"
     },{
         "desc":"Learn Angular"
-    },
-               {
-                   "desc":"Complete HTML,CSS"
-               }];
+    }, {
+        "desc":"Complete HTML,CSS"
+    }];
     function addTask(){
-        $scope.tasks.push($scope.todoTask);
+        $scope.errortext = "";
+        if (JSON.stringify($scope.tasks).indexOf($scope.todoTask.desc) == -1) {
+            $scope.tasks.push($scope.todoTask);
+        } else {
+            $scope.errortext = "The Task is already in your Todo List.";
+        }       
         $scope.todoTask={};
     };
+
     $scope.tasks=tasks;
     $scope.addTask = addTask;
 });
