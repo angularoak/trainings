c15b4eb7695dc8bf6adec7b888f08c07d9e152a8
dharmendhar kumar <dharmendhar kumar>
2016-05-02 07:50:44 +0530

    01052016

--- README.md
+++ README.md
@@ -3,14 +3,19 @@ This is a Angular trainging repository
 to get the repository follow these steps
 
 git init
+
 git remote add origin https://github.com/angularoak/trainings.git
+
 git pull origin master
 
 Add you changes to the source code then use followig commands
+
 git add .
+
 git commit -m "<Some message>"
 
 To update changes to repo
+
 git push origin master
 
 To get the changes from repo
---
+++ Students/Dharma/js/http_example.js
@@ -0,0 +1,20 @@
+var app = angular.module("http_example",[]);
+
+app.controller("OrderDetails",function($scope,$http){
+function Sucess(response) {
+    $scope.orders = response.data.value;
+    };
+    
+     function faliure(error){
+       $scope.httpError="Somthing Went wrong please contact Admin"
+   };
+    
+    $http.get("http://services.odata.org/V4/Northwind/Northwind.svc/Orders").then(Sucess,faliure);
+    
+     function searchHearFunction(value){
+        console.log(value.CustomerID);
+        }
+    $scope.searchHear = searchHearFunction;
+    $scope.getDetailsOfProduct= function(value){
+        $scope.CustName= value.CustomerID
+    }
