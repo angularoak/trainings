ba0e3b019f491605673ab00380e9346ee12ee1d2
dharmendhar kumar <dharmendhar kumar>
2016-05-04 22:53:18 +0530

    routing example modified

--- routing with bootstrap and angularjs/js/routing.js
+++ routing with bootstrap and angularjs/js/routing.js
@@ -29,11 +29,13 @@ app.config(
     when('/todo', {
         templateUrl: 'navlinks/todo.html',
         controller: 'todoController'
+         
       }).
       otherwise({
         redirectTo: '/home'
       });
-  });
+   });
+
 app.controller("homeController",function($scope){
    // $scope.Message="Home Page"
 });
@@ -47,39 +49,20 @@ app.controller("httpController",function($scope){
    // $scope.Message="Home Page"
 });
 
-app.controller("homeController",function($scope){
+app.controller("localStorageController",function($scope){
    // $scope.Message="Home Page"
 });
 
-app.controller("homeController",function($scope){
+app.controller("modelExampleController",function($scope){
    // $scope.Message="Home Page"
 });
 
-app.controller("homeController",function($scope){
+app.controller("ngReaptExampleController",function($scope){
    // $scope.Message="Home Page"
 });
 
-app.controller("homeController",function($scope){
+app.controller("todoController",function($scope){
    // $scope.Message="Home Page"
 });
-app.controller("ordersController",function($scope,$http){
-    $scope.Message="List of Orders";
-    $scope.myorderby = function(value){
-        alert(value);
-    }
-    function Sucess(response) {
-    $scope.orders = response.data.value;
-    };
-   function faliure(error){
-       $scope.httpError="Somthing Went wrong please contact Admin"
-   }; 
-    $http.get("http://services.odata.org/V4/Northwind/Northwind.svc/Orders").then(Sucess,faliure);
-  
-    
-});
-app.controller("aboutUsController",function($scope){
-    $scope.Message="About us  Page"
-});
-app.controller("contactController",function($scope){
-    $scope.Message="Contact us  Page"
-});
+      
+       
