86564ea7a26f530c54f05783330f14b22e599c14
swarna <angularjsoak@gmail.com>
2016-05-23 09:19:24 +0530

    login logic

--- Project Spec/swarna/js/app.js
+++ Project Spec/swarna/js/app.js
@@ -1,1 +1,17 @@
-var app = angular.module("sgaApp",[]);
+var app = angular.module("sgaApp", ['ngRoute']);
+
+app.config(
+    function ($routeProvider) {
+        $routeProvider.
+        when('/home', {
+            templateUrl: 'tempaltes/home.html'
+            , controller: 'homeController'
+        }).
+        when('/login', {
+            templateUrl: 'tempaltes/login.html'
+            , controller: 'loginctrl'
+        }).
+        otherwise({
+            redirectTo: '/login'
+        });
+    });
--- Project Spec/swarna/js/controllers/loginCtrl.js
+++ Project Spec/swarna/js/controllers/loginCtrl.js
@@ -1,14 +1,34 @@
-app.controller("loginctrl", function($scope,userDetails) {
+app.controller("loginctrl", function ($scope, userDetails) {
     function login() {
         function userSuccess(responce) {
+            var invalidUser = true;
             $scope.users = responce.data.users;
-            
+            angular.forEach($scope.users, function (value, key) {
+                if (invalidUser) {
+                    if ($scope.uname === value.userName && $scope.password === value.password) {
+                        $scope.Message = "Valid User";
+                        invalidUser = false;
+                    } else {
+                        invalidUser = true;
+                        $scope.Message = "Invalid User, Please try again";
+                    }
+                }
+            });
+            /* Plain JS style            
+            for (var i = 0, len = $scope.users.length; i < len; i++) {
+                 if ($scope.uname === $scope.users[i].userName) {
+                     console.log('Loop is going to break.');
+                     break;
+                 }
+                 console.log('Loop will continue.');
+             }*/
+
         };
 
-        function userFailure(responce) {
+        function userFailure(error) {
             alert("failed to load details");
         };
-        userDetails.get(userSuccess,userFailure);
+        userDetails.get(userSuccess, userFailure);
 
     };
 
