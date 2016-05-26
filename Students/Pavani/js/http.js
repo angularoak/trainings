7cdb298e34f01a910d4143849bd2ca21c0a07472
Suneel Manyam <angularjsoak@gmail.com>
2016-05-26 08:18:10 +0530

    Pavani backup classes

--- Students/Pavani/js/http.js
+++ Students/Pavani/js/http.js
@@ -1,10 +1,15 @@
 var app = angular.module("httpExample", []);
 
 app.controller("productsDataController", function($scope,$http) {
+    $scope.orderBydescAsc = true;
     function productsSucess(resultSet) {
         $scope.products = resultSet.data.value;
     };
     function productsFailed(error) {
         $scope.httpError = "Somthing Went wrong please contact Admin"
     };    $http.get("http://services.odata.org/V4/Northwind/Northwind.svc/Products").then(productsSucess, productsFailed);
+    $scope.sort = function(sortBy){
+        $scope.sortBy=sortBy;
+        $scope.orderBydescAsc=!$scope.orderBydescAsc;  
+    }
    });
